9bc8db548b5fd54fc071a4c2854a868aada7cb7b
Kartikeya <kudlu2004@gmail.com>
2024-05-16 22:58:44 +0530

    Removed the qr code

--- app/donate/page.js
+++ app/donate/page.js
@@ -28,7 +28,7 @@ function Donate() {
         <h1 className="text-primary text-center text-4xl font-bold">
           {globalData?.donatePageTitle ?? "Donate"}
         </h1>
-        <p className="text-center mt-2 text-muted-foreground">
+        <p className="text-center mt-2 text-muted-foreground max-w-[900px]">
           {globalData?.donatePageDescription ?? "No description"}
         </p>
       </div>
@@ -37,45 +37,16 @@ function Donate() {
         <div className="w-full max-w-[600px]">
           <DonateForm addDonationToFirestore={addDonationToFirestore} />
         </div>
-        <div className="w-full max-w-[710px] flex flex-col items-center ">
+        <Separator className="" />
+        <div className="flex flex-col items-center">
           <h2 className="text-center flex items-center text-2xl underline underline-offset-8">
-            Donate via QR Code
+            Terms & Conditions
           </h2>
-          <div className="mt-10 flex items-center justify-around border rounded-lg p-4 bg-card">
-            <div className="border p-4 rounded-lg bg-muted">
-              <img src={globalData?.qrCodeDetails.image} alt="qr-code" />
-            </div>
 
-            <div className="flex flex-col items-start justify-around max-w-[300px] sm:ml-10 w-full">
-              <div>
-                <h2 className="text-2xl mt-4 font-semibold text-center">
-                  {globalData?.qrCodeDetails.name ?? "Upasana Foundation"}
-                </h2>
-                <p className="text-center mt-2 text-muted-foreground">
-                  {globalData?.qrCodeDetails.phone ?? "Description"}
-                </p>
-
-                <p className="mt-2 text-center">
-                  {globalData?.qrCodeDetails.shortMessage}
-                </p>
-              </div>
-              <div className="p-2 flex justify-center w-full mt-6 border rounded-lg bg-muted">
-                <p>UPI: {globalData?.qrCodeDetails.upiId}</p>
-              </div>
-            </div>
-          </div>
-
-          <Separator className="my-10" />
-          <div className="flex flex-col items-center">
-            <h2 className="text-center flex items-center text-2xl underline underline-offset-8">
-              Terms & Conditions
-            </h2>
-
-            <div className="mt-10 flex flex-col gap-4 items-center">
-              {globalData?.donationTermsAndConditions?.map((item, index) => (
-                <TermsAndConditionsItem key={index} text={item} />
-              ))}
-            </div>
+          <div className="mt-10 flex flex-col gap-4 items-center">
+            {globalData?.donationTermsAndConditions?.map((item, index) => (
+              <TermsAndConditionsItem key={index} text={item} />
+            ))}
           </div>
         </div>
       </div>
--- components/TermsAndConditionsItem.jsx
+++ components/TermsAndConditionsItem.jsx
@@ -4,9 +4,12 @@ import { BiCheck } from "react-icons/bi";
 function TermsAndConditionsItem({ text }) {
   return (
     <div>
-      <div className="flex gap-2 items-center ">
-        <BiCheck className="text-primary" size={25} />
-        <p className="text-muted-foreground">{text}</p>
+      <div className="flex gap-2 items-center flex-grow relative">
+        <BiCheck
+          className="text-primary text-xl h-6 w-6 absolute left-0 top-0"
+          size={25}
+        />
+        <p className="text-muted-foreground ml-8">{text}</p>
       </div>
     </div>
   );
